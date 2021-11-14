f8ed8d0ddac9566623891245c28bf963d724e658
Sergay <tsvetkoffgolod@yandex.ru>
2021-11-15 01:13:21 +0300

    adapting popups for responsiveness, fixing errors in the console

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,88 @@
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../index.css';
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm'
+import React from "react";
 
 function App() {
+
+
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState();
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState();
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState();
+
+
+  function openAddPopup() {
+    setIsAddPlacePopupOpen(true)
+  }
+  function openProfilePopup() {
+    setIsEditProfilePopupOpen(true)
+  }
+  function openAvatarPopup() {
+    setIsEditAvatarPopupOpen(true)
+  }
+  function closeAllPopups() {
+    setIsAddPlacePopupOpen(false)
+    setIsEditProfilePopupOpen(false)
+    setIsEditAvatarPopupOpen(false)
+  }
+
+
   return (
-<body className="page">
+<div className="page">
 
 <Header />
-<Main />
+<Main
+  onAddPlace  = {openAddPopup}
+  onEditProfile = {openProfilePopup}
+  onEditAvatar = {openAvatarPopup}
+/>
 <Footer />
 
+<PopupWithForm
+  isOpen = {isAddPlacePopupOpen}
+  onClose = {closeAllPopups}
+  title = {'Новое место'}
+  buttonText = {'Создать'}
+  children = {(
+    <>
+      <input id="input-title" className="popup__input popup__input_image_name" type="text" name="inputTitle" placeholder="Название" minLength="2" maxLength="30" required />
+      <span id="input-title-error"></span>
+      <input id="input-link" className="popup__input popup__input_link" type="url" name="inputImg" required placeholder="Ссылка на картинку" />
+      <span id="input-link-error"></span>
+    </>
+  )}
+/>
+<PopupWithForm
+  isOpen = {isEditProfilePopupOpen}
+  onClose = {closeAllPopups}
+  title = {'Редактировать профиль'}
+  buttonText = {'Сохранить'}
+  children = {(
+    <>
+      <input id="input-name" className="popup__input popup__input_type_name" type="text" name="inputName" required placeholder="Имя" minLength="2" maxLength="40" />
+      <span id="input-name-error"></span>
+      <input id="input-job" className="popup__input popup__input_type_job" type="text" name="inputJob" required placeholder="Работа" minLength="2" maxLength="200" />
+      <span id="input-job-error"></span>
+    </>
+  )}
+/>
+<PopupWithForm
+  isOpen = {isEditAvatarPopupOpen}
+  onClose = {closeAllPopups}
+  title = {'Обновить аватар'}
+  buttonText = {'Сохранить'}
+  children = {(
+    <>
+      <input id="input-avatar" className="popup__input popup__input_link" type="url" name="avatarInpur" required placeholder="Ссылка на аватар" />
+      <span id="input-avatar-error"></span>
+    </>
+  )}
+/>
 
-<div className="popup popup-edit">
+{/* <div className="popup popup-edit">
   <div className="popup__container">
     <h2 className="popup__title">Редактировать профиль</h2>
     <form className="popup__form" name="popup-edit" >
@@ -87,7 +156,7 @@ function App() {
   </article>
 </template>
 
-{/* <div className="popup popup-card">
+<div className="popup popup-card">
   <div className="popup__wrapper">
     <button type="button" className="popup__close-card popup__close-button">
       <img className="popup__image" src="<%=require('./images/close-btn.svg')%>" alt="Закрыть" />
@@ -100,7 +169,7 @@ function App() {
   </div>
 </div> */}
 
-</body>
+</div>
   );
 }
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,19 +2,14 @@ import closeBtn from '../images/close-btn.svg'
 
 function PopupWithForm(props) {
   return (
-    <div className={`popup popup-${props.name}`}>
+    <div className={`popup popup-${props.name} ${props.isOpen ? 'popup_opened' : ''}`} >
       <div className="popup__container">
         <h2 className="popup__title">{props.title}</h2>
         <form className="popup__form" name={`popup-${props.name}`} >
           {props.children}
-
-          {/* <input id="input-name" className="popup__input popup__input_type_name" type="text" name="inputName" required placeholder="Имя" minlength="2" maxlength="40" />
-          <span id="input-name-error"></span>
-          <input id="input-job" className="popup__input popup__input_type_job" type="text" name="inputJob" required placeholder="Работа" minlength="2" maxlength="200" />
-          <span id="input-job-error"></span>
-          <button className="popup__button" type="submit">Сохранить</button> */}
+          <button className="popup__button" type="submit">{props.buttonText}</button>
         </form>
-        <button type="button" className="popup__close-button">
+        <button type="button" className="popup__close-button" onClick={props.onClose} >
           <img className="popup__image" src={closeBtn} alt="Закрыть" />
         </button>
       </div>
